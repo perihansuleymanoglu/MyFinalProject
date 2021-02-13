42617741bf4e3d7e23f225a1be3ec7bfc1255385
Perihan Süleymanoğlu <47482913+perihansuleymanoglu@users.noreply.github.com>
2021-02-13 22:51:16 +0300

    13Ocak

---
+++ Business/Concrete/ErrorDataResult.cs
@@ -0,0 +1,29 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Business.Concrete
+{
+    internal class ErrorDataResult<T> : DataResult<T>
+    {
+        public ErrorDataResult(T data, string message) : base(data, false, message)
+        {
+
+        }
+
+        public ErrorDataResult(T data) : base(data, false)
+        {
+
+        }
+
+        public ErrorDataResult(string message) : base(default, false, message)
+        {
+
+        }
+
+        public ErrorDataResult() : base(default, false)
+        {
+
+        }
+    }
+}
